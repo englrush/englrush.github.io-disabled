3ebc1bd4faafb756102621cf6805d5ae00df1910
vlad <vlacldev@gmail.com>
2022-07-10 11:18:23 +0200

    require('mongoose')

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
-const app=express()
+const app = express()
+
+mongoose
+    .connect('mongodb+srv://admin:admin123@cluster0.h5kmq.mongodb.net/?retryWrites=true&w=majority')
+    .then((res) => console.log("Connected to MongoDB"))
+    .catch((e) => console.log(e))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log("http://localhost:5000"))
