0e6c5a7f01e131b365969cea7bdb3265a6566a1b
vlad <vlacldev@gmail.com>
2022-07-18 20:10:21 +0200

    + put and delete methods

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Topic = require('./models/topic')
 require('dotenv').config()
 
@@ -14,6 +15,8 @@ app.listen(process.env.PORT, () => console.log("http://localhost:" + process.env
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
+
+app.use(methodOverride('_method'))
 
 app.use(express.urlencoded({ extended: false }))
 
@@ -30,6 +33,42 @@ app.get('/topics:id', (req, res) => {
         .catch((e) => {
             console.log(e)
             res.render('error', { title: 'Error' });
+        })
+})
+
+app.delete('/topics:id', (req, res) => {
+    const title = "Delete Topics"
+    Topic
+        .findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.sendStatus(200)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.render('error', { title: 'Error' });
+        })
+})
+
+app.get('/edit:id', (req, res) => {
+    const title = "Edit Topics"
+    Topic
+        .findById(req.params.id)
+        .then((topic) => res.render('edit-topic', { title, topic }))
+        .catch((e) => {
+            console.log(e)
+            res.render('error', { title: 'Error' });
+        })
+})
+
+app.put('/edit:id', (req, res) => {
+    const { title, author, text } = req.body
+    const { id } = req.params
+    Topic
+        .findByIdAndUpdate(id, { title, author, text })
+        .then(result => res.redirect(`topics${id}`))
+        .catch((e) => {
+            console.log(e)
+            res.render(e);
         })
 })
 
